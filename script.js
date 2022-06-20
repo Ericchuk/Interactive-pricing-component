204a9452d214dcfeb67c4a99a9c119be4a28927e
Ericchuk <47712998+Ericchuk@users.noreply.github.com>
2022-06-20 02:18:52 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 let slider = document.getElementById("myRange");
 let output = document.getElementById("value");
-output.innerHTML = slider.value;
+let valueChange = document.querySelector("b");
+let priceRange = document.querySelector(".amount");
 
-slider.oninput = function(){
-    output.innerHTML = this.value;
-}
 
 slider.addEventListener("mousemove", function(){
     let x = slider.value;
-    let color = `linear-gradient(90deg, var(--full-slider-bar) ${x}%, var(--empty-slider-bar) ${x}%)`;
+    let color = `linear-gradient(90deg, var( --slider-background) ${x}%, var(--empty-slider-bar) ${x}%)`;
     slider.style.backgroundImage = color;
-    console.log(x)
-    console.log(color);
+    if(x == 10){
+        console.log(x);
+        valueChange.innerHTML = "10k";
+        priceRange.innerHTML = 8;
+    }else if(x == 30){
+        console.log(x);
+        valueChange.innerHTML = "50k";
+        priceRange.innerHTML = 12;
+    }else if(x == 50){
+        console.log(x);
+        valueChange.innerHTML = "100k";
+        priceRange.innerHTML = 16;
+    }else if(x == 70){
+        console.log(x);
+        valueChange.innerHTML = "500k";
+        priceRange.innerHTML = 24;
+    }else if(x == 100){
+        console.log(x);
+        valueChange.innerHTML = "1M";
+        priceRange.innerHTML = 36;
+    };
+    // console.log(x)
+    // console.log(color);
 });
 
